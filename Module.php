80efa8c56ec98c06578692868116f58f5a842b0c
Igor Panasenko <igor@afterlogic.com>
2024-01-10 14:28:25 +0300

    Add password reset functionality

--- Module.php
+++ Module.php
@@ -87,7 +87,8 @@ class Module extends \Aurora\System\Module\AbstractModule
         $bBreakSubscriptions = false;
 
         $oAccount = $aArguments['Account'];
-        if ($oAccount && $this->checkCanChangePassword($oAccount) && $oAccount->getPassword() === $aArguments['CurrentPassword']) {
+        if ($oAccount && $this->checkCanChangePassword($oAccount) && ($oAccount->getPassword() === $aArguments['CurrentPassword']
+          || isset($aArguments['SkipCurrentPasswordCheck']) && $aArguments['SkipCurrentPasswordCheck'])) {
             $bPasswordChanged = $this->changePassword($oAccount, $aArguments['NewPassword']);
             $bBreakSubscriptions = true; // break if mail server plugin tries to change password in this account.
         }
